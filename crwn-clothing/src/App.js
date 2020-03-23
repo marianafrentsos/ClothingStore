c356e013a222d79cecd98f9c8004eeb766642963
Mariana Frentsos <marianafrentsos@yahoo.com>
2020-03-23 23:08:54 +0200

    added logo and links

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import ShopPage from "./pages/ShopPage/ShopPage.jsx";
 import "./App.css";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
+
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
