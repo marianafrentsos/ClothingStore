d1680d9d096a9a3acc4a68882f09c43b189854f7
Mariana Frentsos <marianafrentsos@yahoo.com>
2020-03-27 16:19:46 +0200

    heroku on git

--- src/Firebase/Firebase.utils.js
+++ src/Firebase/Firebase.utils.js
@@ -1,25 +1,18 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
 import "firebase/auth";
-require("dotenv").config();
-export const API_KEY = process.env.REACT_APP_API_KEY;
-export const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
-export const DATABASE_URL = process.env.REACT_APP_DATABASE_URL;
-export const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
-export const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
-export const SENDER_ID = process.env.REACT_APP_SENDER_ID;
-export const APP_ID = process.env.REACT_APP_APP_ID;
-export const MEASSUREMENT_ID = process.env.REACT_APP_MEASSUREMENT_ID;
+import "firebase/firestore";
 
-const config = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASSUREMENT_ID
+require("dotenv").config();
+
+export const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASSUREMENT_ID
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
