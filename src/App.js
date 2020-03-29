e2a0ba3a3109c75237e7129b2218d128ddb077d7
Mariana Frentsos <marianafrentsos@yahoo.com>
2020-03-29 11:19:10 +0300

    impelemented firebase utils, included ability to store authenticated users into the firestore database

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ class App extends React.Component {
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
   render() {
     return (
       <div>
--- src/pages/SignIn_SignUp/SignIn_SigUp.jsx
+++ src/pages/SignIn_SignUp/SignIn_SigUp.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SignIn_SignUp.scss";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 export default function SignIn_SigUp() {
   return (
     <div className="signIn_signUp">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
