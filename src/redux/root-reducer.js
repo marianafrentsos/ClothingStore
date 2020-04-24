7a240a4802f1dfcc40394d9bcb8520102a641ec7
Mariana Frentsos <marianafrentsos@yahoo.com>
2020-04-24 15:16:22 +0300

    installed npm persist, and use localStorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./User/userReducer.js";
 import cartReducer from "./Cart/cartReducer.js";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
