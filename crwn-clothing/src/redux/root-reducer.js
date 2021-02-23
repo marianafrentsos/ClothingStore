b28e2ca9ae6f044db840d3de7ccaa1b12e455573
Mariana Frentsos <marianafrentsos@yahoo.com>
2021-02-23 21:06:37 +0200

    removed logger from prod build

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./User/userReducer.js";
 import cartReducer from "./Cart/cartReducer.js";
+import directoryReducer from "./Directory/directoryReducer";
+import shopReducer from "./Shop/shopReducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
